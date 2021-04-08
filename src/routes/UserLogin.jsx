a4d033744e0dbf224c3aaaa5d3181ac457c86b4d
humpty dumpty <spyker5@creationuq.com>
2021-04-08 13:23:30 +0300

    styled login page

--- src/routes/UserLogin.jsx
+++ src/routes/UserLogin.jsx
@@ -2,46 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../js/actions';
 import { withRouter } from 'react-router-dom';
+import '../css/UserLogin.scss';
 
 let $ = require('jquery');
 
 function UserLogin(props) {
     return (
-        <div> rtesgsregsregs
-            <form
+        <div id="user-login"> 
+            <form id="login-form"
                 onSubmit={curryHandleSubmit(props)}
             >
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
+                <h1 id="sign-in-label">Sign In</h1>
 
                 <input id="email-field"
                     type="email"
+                    placeholder="Email:"
                 />
-
-
                 <input id="password-field"
                     type="password"
+                    placeholder="Password:"
                 />
-
-
                 <input
                     type="submit"
                     value="Log in"
                 />
-
             </form>
+
+            <div id="or-signin-with">
+                <p className="gray-text">Or</p>
+                <p className="gray-text">Sign in with</p>
+            </div>
+
+            <input id="login-facebook" className="social-media-signin" type="button" value="Facebook"/>
+            <input id="login-google" className="social-media-signin" type="button" value="Google"/>
+            <input id="login-twitter" className="social-media-signin" type="button" value="Twitter"/>
+
+            <div id="or-signup">
+                <p className="gray-text">Or</p>
+                <p id="sign-up" className="gray-text">Sign up</p>
+                <p className="gray-text">for free</p>    
+            </div>
         </div>
     )
 }
@@ -64,16 +64,19 @@ function curryHandleSubmit(props){
             data: credentials,
             success: response => {
 
-                handleLogIn(props);
+                handleLogIn(props, response);
             }
         });
     }
 }
 
-const handleLogIn = (props) => {
-    props.logInUser(true);
-
-    props.history.push("/"); //needs {withRouter}
+const handleLogIn = (props, response) => {
+    if(response === true){
+        props.logInUser(true);
+        props.history.push("/"); //needs {withRouter}
+    } else {
+        console.log("incorrect or missing credentials")
+    }
 }
 
 const mapStateToProps = (state) => {
