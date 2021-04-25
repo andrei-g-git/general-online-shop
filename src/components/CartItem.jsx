5939feb44d4190b8ec7d15760a0066c977c5c67e
humpty dumpty <spyker5@creationuq.com>
2021-04-25 15:05:33 +0300

    fixed footer ovelapping with the content

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,6 +4,7 @@ import minusIcon from '../assets/icons/minus_white.png';
 import plusIcon from '../assets/icons/plus_white.png';
 
 function CartItem(props) {
+
     return ( //still needs euro suffix for most quantities
         props.quantity ? 
             <div className="cart-item"> 
@@ -32,7 +33,15 @@ function CartItem(props) {
                     <label>Shipping:</label>
                     <p>{props.currencyPrefix}8.50</p>
                     <label>Subtotal:</label>
-                    <p>{props.currencyPrefix}{props.price - props.discount + 8.50}</p>
+                    {
+                        //something strange happens if the discount price is 0, the decimals go crazy ... this is a 
+                        //stop-gap-solution, the final numbers should be retrieved with mysql (at least don't do arithmetic here)
+                        props.discount > 0 ?
+                            <p>{props.currencyPrefix}{props.price - props.discount + 8.50}</p> //STILL DOESN"T WORK
+                        :
+                            <p>{props.currencyPrefix}{props.price + 8.50}</p>
+                    }
+                    
                 </div>
 
 
