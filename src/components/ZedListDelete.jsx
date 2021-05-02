05c11e92dad0cfccf39f4ec64d701c905dac98ff
humpty dumpty <spyker5@creationuq.com>
2021-05-02 18:11:13 +0300

    Revert "Revert "started adding bootstrap, broke some stuff""
    This reverts commit 586bde207543d1c539561cec057f93d75278d30d.

---
+++ src/components/ZedListDelete.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function zListDelete() {
+    return (
+        <div>
+        <ul style={{zIndex: "9999"}}>
+            <li>list element</li>
+            <li>list element</li>
+            <li>list element</li>
+            <li>list element</li>
+            <li>list element</li>
+            <li>list element</li>
+            <li>list element</li>
+        </ul>
+    </div>
+    )
+}
+
+export default zListDelete;
