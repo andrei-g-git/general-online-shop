6c39a22169948b416a6090b47fed0c24ea30a6eb
humpty dumpty <spyker5@creationuq.com>
2021-05-02 18:11:13 +0300

    Revert "Revert "fixed footer ovelapping with the content""
    This reverts commit b472c7191e974427b9aa43e5a4bdae40c0eed34c.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import { makeStore } from './js/makeStore';
 import { BrowserRouter } from 'react-router-dom';
 
+//import './App.css';
+
 const store = makeStore();
 
 function App() {
