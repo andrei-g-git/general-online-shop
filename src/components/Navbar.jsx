b33ea14e66f9dec9ea2e80ca2463c05dbe58f2e9
humpty dumpty <spyker5@creationuq.com>
2021-05-03 19:42:48 +0300

    Revert "Revert "removed bootstrap from navbar""
    This reverts commit 823844ffd9e4cd7d00fbd09f78e6aa5c82f84df3.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,7 @@ import SiteLogo from './SiteLogo';
 import Search from './Search';
 import CartIcon from './CartIcon';
 import UserLoginIcon from './UserLoginIcon';
-//import '../css/Navbar.scss';
-
-//test
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/Navbar.scss';
 
 function Navbar(props) {
     let navSliderId = "nav-slider";
@@ -20,28 +17,16 @@ function Navbar(props) {
     return (
         
         <div>
-            <nav className="navbar navbar-default" 
+            <nav
                 id="nav-bar"
             >
-                    <HamburgerMenu></HamburgerMenu>
-
-                <div className="navbar-brand">
-                    <SiteLogo></SiteLogo>
-
-                </div> 
-
-                <div className="nav-item"></div>
-                {/* <nav id="right-hand-icons"> */}
-                    <div className="nav-item">
-                        <Search></Search>
-                    </div>
-                    <div className="nav-item">
-                        <CartIcon></CartIcon>
-                    </div>
-                    <div className="nav-item">
-                        <UserLoginIcon></UserLoginIcon>
-                    </div>
-                {/* </nav> */}
+                <HamburgerMenu></HamburgerMenu>
+                <SiteLogo></SiteLogo>
+                <div id="right-hand-icons">
+                    <Search></Search>
+                    <CartIcon></CartIcon>
+                    <UserLoginIcon></UserLoginIcon>
+                </div>
             </nav>
 
             <div id={sliderContainerId}> 
@@ -51,7 +36,7 @@ function Navbar(props) {
                     <h1>Stuff</h1>
                 </ul>
             </div>
-        </div>
+    </div>
     )
 }
 
